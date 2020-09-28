7875d79f04a03078b8900e6e270f36b0951c73a8
messaoud795 <messaoudnabil9@gmail.com>
2020-09-28 13:52:35 +0200

    24 commit

---
+++ App.js
@@ -0,0 +1,82 @@
+const mongoose=require("mongoose");
+// connect to mongod server and create contactDB database
+mongoose.connect("mongodb://localhost:27017/listDB", { useNewUrlParser: true , useUnifiedTopology: true});
+
+//Connection check to mongodb server
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("we're connected to the database server") ; 
+});
+//create the schema
+personSchema=new mongoose.Schema({
+ name : {type:String, required:[true, "Please enter a name"]},
+ age  : Number,
+  favoriteFoods :  [{type: String}]
+})
+// Create the model
+const Person=mongoose.model("Person", personSchema);
+//add record to our database
+const arrayOfPeople=[
+{name: "John" , age: 26 , favoriteFoods: ["Banana", "chiken", "meat"]} ,
+{name: "Mary" , age: 52 , favoriteFoods: ["Salad", "apple", "fish"]} ,
+{name: "Sam" , age: 40 , favoriteFoods: ["Apple", "chiken", "burrito"]} ,
+{name: "Adam" , age: 62 , favoriteFoods: ["Cake", "burrito", "riz"]} ,
+{name: "Mary" , age: 23 , favoriteFoods: ["Cake", "choclate", "honey"]} ,
+{name: "John" , age: 50 , favoriteFoods: ["Strawberry", "burrito", "meat"]}];
+
+// count the number of records in listDB  
+Person.countDocuments({}, (error, count)=>{
+  if (error) {console.log(error)}
+  else  { doc(count)}})
+
+//Add docs only the fist time when the num of docs =0
+function doc(n) {
+  console.log(n);
+  if (n==0){
+    Person.insertMany(arrayOfPeople, (err, data)=>{
+      if (err){console.log(err)}
+      else {    mongoose.connection.close();
+        console.log("docs successfully saved to the DB"+ data);
+        return (null, data)}}) }
+  
+}
+// find all the people with name of  John
+Person.find({ name: 'John'}, (error,data)=>{
+  if (error){console.log(error)}
+  else {
+    console.log("Found Johns" + data);
+  }
+});
+
+//find all the first John
+Person.findOne({ name: 'John'}, (error,data)=>{
+  if (error){console.log(error)}
+  else {
+    console.log("the first John found "+data);
+  }
+});
+
+//update a doc found attribute age to 20
+Person.updateOne({name:"Adam"},{age:20},(err)=>
+err? console.log(err):console.log("successfully updated"));
+//delete the docs with name of Mary
+  Person.deleteMany({name:"Mary"}, (error)=>{
+  if (error){console.log(error)}
+  else {console.log("sucessfully removed")}
+})
+//find the two people who like burrito and sort them by age descendtly 
+Person.find({favoriteFoods: { $all : ["burrito"]}}).sort({'age':-1}).limit(2).exec((error,data)=>{
+  if (error){console.log(error)}
+  else {
+    console.log("two People  like burrito "+data); }})
+
+//find doc by id and update the favoritefood attribute
+Person.findById("5f6fae0d0b9df405b477a7f8", (error,data)=>{
+  if (error){console.log(error)}
+  else {data.favoriteFoods.push("Hamburger");
+    console.log("doc found by id  "+ data);
+  }
+});
+
+
